4d55905d22d6d1b2048fd0e50b5867f4b9c66979
Deepto <ddepp98@outlook.com>
2022-12-30 21:17:56 +0000

    completed MAC commands

--- 2-changing-mac/main.go
+++ 2-changing-mac/main.go
@@ -8,11 +8,18 @@ import (
 
 func main() {
 	selectedInterface := "eth0"
+
+	//Change MAC
+	//1. Shut down interface
 	runCommand("sudo", "ifconfig", selectedInterface, "down")
+	//2. Change MAC
+	runCommand("sudo", "ifconfig", selectedInterface, "hw", "ether", "60:45:bd:1e:8d:fe")
+	//3. Turn on interface
+	runCommand("sudo", "ifconfig", selectedInterface, "up")
 
 }
 
-//Variadic function to accept arguments
+// Variadic function to accept arguments
 func runCommand(cmd string, args ...string) error {
 
 	commandObj := exec.Command(cmd, args...)
